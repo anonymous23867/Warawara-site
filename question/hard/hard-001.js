03dc466a1d7d208b12bdbfd79c8cd5c285d24ea3
anonymous23867 <greenkigyou@gmail.com>
2026-05-15 14:30:00 +0900

    fix username localStorage keys

--- question/hard/hard-001.js
+++ question/hard/hard-001.js
@@ -2,10 +2,34 @@ window.quizDataParts = window.quizDataParts || {};
 window.quizDataParts.hard = window.quizDataParts.hard || [];
 
 window.quizDataParts.hard.push(
-  {
-      question: "かつて検索すると、ゾンビのような見た目をしている映画撮影用の人形の画像がヒットしていたワードは？",
+  　　{　　
+    　question: "かつて検索すると、ゾンビのような見た目をしている映画撮影用の人形の画像がヒットしていたワードは？",
       media:"images/IMG_7112.jpg",
       choices: ["メキシコ　人形","adult dolly","隣の家のイザベル","トモミ"],
       answer: 3
-    }
+    },
+    {
+      question: "検索すると、自転車に乗った女の子が男性を轢いているカオスな交通事故注意看板の画像がヒットする言葉は？",
+      media:"images/IMG_7391.JPG",
+      choices: ["危い","とびだしゃみえる","幼稚園　自画像　スレ","あはははは"],
+      answer: 0
+    },
+    {
+      question: "検索すると、アフリカのヴィクトリア湖周辺で大量発生した蚊を集めて料理をする衝撃的な内容がヒットする言葉は？",
+      media:"images/IMG_7392.JPG",
+      choices: ["貴重なタンパク源です", "アフリカ　ハンバーグ", "ディスカバリークッキング", "ヴィクトリア　タンパク源"],
+      answer: 1
+    },
+    {
+      question: "検索すると、アメリカ人女性が「ケリー」というゾンビ人形と結婚したという記事や、不気味な人形の画像がヒットする言葉は？",
+      media:"images/IMG_7394.jpg",
+      choices: ["孫娘　人形", "ケリー　結婚", "アメリカ　人形と結婚", "アメリカ　ケリー　ゾンビ"],
+      answer: 2
+    },
+    {
+      question: "検索すると、母親が娘を電子レンジに入れたという海外の事件記事や、火傷を負った少女の画像がヒットする言葉は？",
+      choices: ["電子レンジ　てんかん", "ディープサウス州　電子レンジ", "ファービー　電子レンジ", "アラバマ州　電子レンジ"],
+      answer: 3
+    },
+
 );
